58124f096fcbb1afc30c9fb0880d6c1b074c52e2
Illuminate <60281442+Sarthak-source@users.noreply.github.com>
2023-02-17 11:09:42 +0530

    refs #8880 update on AppPanel radius enum

--- lib/ui/globalcomponents/app_panel.dart
+++ lib/ui/globalcomponents/app_panel.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
+enum AppPanelRadius { xs, sm, md, lg, xl, none }
+
 class AppPanel extends StatefulWidget {
-  final double radius;
+  const AppPanel(
+      {super.key, this.radiusType = AppPanelRadius.md, required this.body});
+
   final Widget body;
-  const AppPanel({super.key, this.radius = 16, required this.body});
+  final AppPanelRadius radiusType;
+  static Map<AppPanelRadius, BorderRadius> radiusMap = {
+    AppPanelRadius.xs: BorderRadius.circular(4.0),
+    AppPanelRadius.sm: BorderRadius.circular(8.0),
+    AppPanelRadius.md: BorderRadius.circular(12.0),
+    AppPanelRadius.lg: BorderRadius.circular(16.0),
+    AppPanelRadius.xl: BorderRadius.circular(20.0),
+    AppPanelRadius.none: BorderRadius.zero,
+  };
+
   @override
   State<AppPanel> createState() => _AppPanelState();
 }
@@ -12,7 +25,7 @@ class _AppPanelState extends State<AppPanel> {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(widget.radius),
+      borderRadius: AppPanel.radiusMap[widget.radiusType],
       child: Scaffold(
         body: widget.body,
       ),
